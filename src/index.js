6e8923cabcee2a61676f37710f09c6ddf1d5f6d4
kiri <2733908676@qq.com>
2024-12-15 21:34:09 +0800

    delete upload

--- src/index.js
+++ src/index.js
@@ -20,22 +20,23 @@ const { APP_PORT } = require("./config/config");
 const router = require("../src/router/index");
 
 // 使用koda-body中间件对body参数进行处理
-app.use(
-  koaBody({
-    // multipart:是否开启文件上传 --> 默认为false
-    multipart: true,
-    formidable: {
-      // 文件要保存的路径
-      //! 在配置选项option中，应该避免使用相对路径 --> 因为在option中，相对的是 process.cwd() 而非当前文件
-      uploadDir: path.join(__dirname, "./upload"),
-      maxFileSize: 200 * 1024 * 1024, // 限制文件大小为 200MB
-      // 保留文件后缀名
-      keepExtensions: true,
-    },
-    // 配置koa-body中间件，使以下方法的body参数可以挂载到ctx.request.body下
-    parsedMethods: ["POST", "PUT", "PATCH", "DELETE"],
-  })
-);
+// app.use(
+//   koaBody({
+//     // multipart:是否开启文件上传 --> 默认为false
+//     multipart: true,
+//     formidable: {
+//       // 文件要保存的路径
+//       //! 在配置选项option中，应该避免使用相对路径 --> 因为在option中，相对的是 process.cwd() 而非当前文件
+//       uploadDir: path.join(__dirname, "./upload"),
+//       maxFileSize: 200 * 1024 * 1024, // 限制文件大小为 200MB
+//       // 保留文件后缀名
+//       keepExtensions: true,
+//     },
+//     // 配置koa-body中间件，使以下方法的body参数可以挂载到ctx.request.body下
+//     parsedMethods: ["POST", "PUT", "PATCH", "DELETE"],
+//   })
+// );
+app.use(koaBody())
 
 // 使用 koa-static 中间件将upload文件夹配置为静态资源
 app.use(koaStaic(path.join(__dirname, "./upload")));
